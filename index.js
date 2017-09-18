a81ad07044405f44e5057153f76f638d948fb630
Korbinian Kuhn <korbiniankuhn@gmail.com>
2017-09-19 00:48:04 +0200

    Export UnauthorizedError

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const middleware = require('./src/middleware');
 const proof = require('./src/proof');
 const ramses = require('ramses-auth');
+const UnauthorizedError = require('./src/errors/UnauthorizedError');
 
 exports.getToken = require('./src/get_token');
 exports.middleware = middleware;
@@ -11,3 +12,4 @@ exports.sign = ramses.sign;
 exports.decode = ramses.decode;
 exports.verify = ramses.verify;
 exports.validate = ramses.validate;
+exports.UnauthorizedError = UnauthorizedError;
